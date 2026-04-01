42efcd5a2b499a71d4a7110ea6f9c43e673c038f
NiharikaLathish <niharikalathish@gmail.com>
2026-04-01 16:08:17 +0530

    final deployment fix

--- app.py
+++ app.py
@@ -14,5 +14,8 @@ def add_data():
 def get_data():
     return jsonify(data_store)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
